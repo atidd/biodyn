45580c36c38e896b405d93edd93945416ff63c26
laurie <laurie@kell.es>
2014-11-11 12:09:47 +0100

    fixed bug in fuxed bug fix

--- R/biodyn-pella.R
+++ R/biodyn-pella.R
@@ -451,7 +451,7 @@ fitPella=function(object,index=index,exeNm='pella',package='biodyn',
 
   if (its<=1){
       i=0
-      bd@diags=dgs=ldply(cpue,function(index){
+      bd@diags=dgs=ldply(index,function(index){
         i=i+1
         stockHat=(stock(bd)[,-dims(stock(bd))$year]+stock(bd)[,-1])/2
         hat     =stockHat*params(bd)[paste("q",i,sep="")]
